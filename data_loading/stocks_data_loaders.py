109c328a02c85e63e25f50fda0c210afaa2aa054
sileye ba <sileye.ba@outlook.com>
2021-12-24 22:36:46 +0100

    add check that input attribute is valid

--- data_loading/stocks_data_loaders.py
+++ data_loading/stocks_data_loaders.py
@@ -1,4 +1,5 @@
 import pandas_datareader.data as pdr
+import logging
 
 def get_stock_data(tickers,start_date,end_date,quandl_api_key):
 	'''
@@ -14,16 +15,20 @@ def get_stock_data(tickers,start_date,end_date,quandl_api_key):
 	stocks_df = pdr.DataReader(tickers,'quandl',start_date, end_date,api_key=quandl_api_key)
 	return stocks_df
 	
-def get_stock_attribute_data(stocks_df, attribute):
+def get_stock_attribute_data(stocks_df,attribute):
 	'''
 	Get stocks attribute data from stocks dataframe
 	inputs:
 	stocks_df: pandas dataframe containing attributes
-	attributes: one of stocks attributes 
+	attribute: one of stocks attributes 
 	AdjClose, AdjHigh, AdjLow, AdjOpen, ExDividend, Close, High, Low, Open, SplitRatio, Volume
 	Outputs:
 	df: dataframe containing specified stocks attribute
 	'''
-	df = stocks_df[attribute]
-	return df
+	if attribute in stocks_df.keys():
+		df = stocks_df[attribute]
+		return df
+	else:
+		logging.debug(f"{attribute} is not a valid attribute")
+	
 	
