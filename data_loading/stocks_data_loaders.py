272023de75bb25c1c787330900cfece8f82feb4b
sileye ba <sileye.ba@outlook.com>
2021-12-24 22:21:03 +0100

    add function to get stocks attribute from stocks dataframe

--- data_loading/stocks_data_loaders.py
+++ data_loading/stocks_data_loaders.py
@@ -1,6 +1,6 @@
 import pandas_datareader.data as pdr
 
-def get_stock_data(tickers, start_date, end_date, quandl_api_key):
+def get_stock_data(tickers,start_date,end_date,quandl_api_key):
 	'''
 	Inputs:
 	tickers: tickers in quandl format: 'WIKI/AAPL' or 'AAPL.US' for Apple stock
@@ -8,10 +8,22 @@ def get_stock_data(tickers, start_date, end_date, quandl_api_key):
 	end_date: history end date in datetime format
 	quandl_api_key: quandl API key you can get to https://data.nasdaq.com/sign-up
 	Ouput:
-	df: pandas datframe containing attributes: 
+	stocks_df: pandas dataframe containing attributes: 
 	AdjClose, AdjHigh, AdjLow, AdjOpen, ExDividend, Close, High, Low, Open, SplitRatio, Volume
 	'''
-
-	df = pdr.DataReader(tickers,'quandl',start_date, end_date,api_key=quandl_api_key)
+	stocks_df = pdr.DataReader(tickers,'quandl',start_date, end_date,api_key=quandl_api_key)
+	return stocks_df
 	
+def get_stock_attribute_data(stocks_df, attribute):
+	'''
+	Get stocks attribute data from stocks dataframe
+	inputs:
+	stocks_df: pandas dataframe containing attributes
+	attributes: one of stocks attributes 
+	AdjClose, AdjHigh, AdjLow, AdjOpen, ExDividend, Close, High, Low, Open, SplitRatio, Volume
+	Outputs:
+	df: dataframe containing specified stocks attribute
+	'''
+	df = stocks_df[attribute]
 	return df
+	
