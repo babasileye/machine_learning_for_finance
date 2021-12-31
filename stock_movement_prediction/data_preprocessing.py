182c31af7c77c4faed1e743b781b3375ce3e46ee
sileye ba <sileye.ba@outlook.com>
2021-12-31 20:08:36 +0100

    Updating preprocessing functions

--- stock_movement_prediction/data_preprocessing.py
+++ stock_movement_prediction/data_preprocessing.py
@@ -4,16 +4,22 @@ import pandas as pd
 def compute_price_movement(current_mean,current_std,next_mean):
     '''
     Compute price movement.
-    If |next_mean-current_mean|<=current_std:no movement (0)
-    If next_mean<current_mean-current_std:movement down (-1)
-    If next_mean>current_mean+next_std:movement up (1)
+    If |next_mean-current_mean|<=current_std:'steady'
+    If next_mean<current_mean-current_std:'downward'
+    If next_mean>current_mean+next_std:'upward'
     '''
     if next_mean<current_mean-current_std:
-        return -1
+        move_label='downward'
+        encoded_move=[1.0,0.0,0.0]
+        return move_label,encoded_move
     elif next_mean>current_mean+current_std:
-        return 1
+        move_label='upward'
+        encoded_move=[0.0,0.0,1.0]
+        return move_label,encoded_move
     else:
-        return 0
+        move_label='steady'
+        encoded_move=[0.0,1.0,0.0]
+        return move_label,encoded_move
 
 def price_preprocessing(price_df,time_period):
     '''
@@ -28,9 +34,9 @@ def price_preprocessing(price_df,time_period):
     "ticker_price": asset-price key
     "normalized_price_sequence": mean-variance normalized prices of a period
     "price_movement":
-        1:next perdiod's mean price is below a standard deviation of the currents periods mean
-        0:next period's mean price is within a standard deviation of the current's period's mean
-        1:next perdiod's mean price is above a standard deviation of the currents periods mean
+        'upward':next perdiod's mean price is below a standard deviation of the currents periods mean
+        'steady':next period's mean price is within a standard deviation of the current's period's mean
+        'uward':next perdiod's mean price is above a standard deviation of the currents periods mean
     '''
     data=[]
     for key in price_df.keys():
@@ -40,14 +46,16 @@ def price_preprocessing(price_df,time_period):
             normalized_prices=[(p-current_mean)/current_std for p in price_df[key][t-time_period:t].tolist()]
             next_mean=price_df[key][t:t+time_period].mean()
             next_std=price_df[key][t:t+time_period].std()
-            price_movement=compute_price_movement(current_mean,current_std,next_mean)
+            move_label,encoded_move=compute_price_movement(current_mean,current_std,next_mean)
             buffer={
                 'ticker_price':key,
                 'normalized_price_sequence':normalized_prices,
                 'sequence_mean':current_mean,
                 'sequence_std':current_std,
                 'next_sequence_mean':next_mean,
-                'price_movement':price_movement}
+                'movement_label':move_label,
+                'onehot_encoded_price_movement':encoded_move
+            }
             data.append(buffer)
             current_mean=next_mean
             current_std=next_std
